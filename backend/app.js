1b9ea5a9d56c3903cced13889b4a1924dc8f24b6
Dylan Lettinga <dletting@mtu.edu>
2017-09-04 02:07:52 -0400

    functioning GET, POST, DELETE

--- backend/app.js
+++ backend/app.js
@@ -34,7 +34,8 @@ app.get('/', function (req, res) {
 /* server artist page */
 app.get('/collection/:cid', function (req, res) {
     console.log("[200] " + req.method + " to " + req.url);
-    mongo.query("artist", {cid: req.param.cid}, function(ret){
+    console.log( req.params.cid);
+    mongo.query("artist", {cid: req.params.cid}, function(ret){
         var page = fs.readFileSync(path.join(__dirname, 'views/artists.html'), "utf8");
         var html = mustache.to_html(page, ret); // replace all of the data
         res.end(html);
@@ -52,20 +53,30 @@ app.get('/api/collection/', function (req, res) {
 })
 
 /* query content of a collection */
-app.get('/api/collection/:cid', function (req, res) {
+app.get('/api/artist/', function (req, res) {
     console.log("[200] " + req.method + " to " + req.url);
-    console.log(req.param.cid);
-    mongo.query("artist", res, {cid: req.param.cid}, function(ret){
+    mongo.query("artist", {}, function(ret){
         res.json(ret);
     });
 })
 
 /* delete collection */
-app.delete('/api/collection/', jsonParser, function (req, res) {
+app.delete('/api/collection/:cid', jsonParser, function (req, res) {
     if (!req.body) return res.sendStatus(400);
     console.log("[200] " + req.method + " to " + req.url);
 
-    mongo.deletedoc("collection", req.body);
+    mongo.deletedoc("collection", {cid: req.params.cid});
+
+    var status = { "status": "[200]", "cid": req.params.cid};
+    res.end(JSON.stringify(status));
+})
+
+/* delete artist */
+app.delete('/api/artist/', jsonParser, function (req, res) {
+    if (!req.body) return res.sendStatus(400);
+    console.log("[200] " + req.method + " to " + req.url);
+
+    mongo.deletedoc("artist", req.body);
 
     var status = { "status": "[200]", "object": req.body};
     res.end(JSON.stringify(status));
